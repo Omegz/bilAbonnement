f343c9ec7895da76085c2e54dd02063af1beed15
Omegz <omarmajed2000@hotmail.com>
2025-12-10 20:16:56 +0200

    index slay

---
+++ src/main/java/com/springmad/bilabonnement/controller/PageController.java
@@ -0,0 +1,18 @@
+package com.springmad.bilabonnement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String indexPage() {
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage() {
+        return "about";
+    }
+}
